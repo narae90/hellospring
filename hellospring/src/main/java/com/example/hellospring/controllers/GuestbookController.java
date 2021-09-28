689433b28e1c346f8602267b8ad1c996e3c41faa
김나래 <narae900@naver.com>
2021-09-28 13:01:29 +0900

    RequestMapping 연습

--- hellospring/src/main/java/com/example/hellospring/controllers/GuestbookController.java
+++ hellospring/src/main/java/com/example/hellospring/controllers/GuestbookController.java
@@ -4,21 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-// 타입 단독 매핑
-// 버전 5부터 사용하지 않는 방식
+//	타입 단독 매핑
+//	버전 5부터 사용하지 않는 방식
 @Controller
 @RequestMapping("/guestbook/*")
 public class GuestbookController {
 	@ResponseBody
-	@RequestMapping
+//	@RequestMapping
 	public String list() {
 		return "<h1>Guestbook:list</h1>";
 	}
 	
 	@ResponseBody
-	@RequestMapping
+//	@RequestMapping
 	public String add() {
-		return "<h1>Guestbook:add</h1>";
+		return "<h1>Guestbook: add</h1>";
 	}
-
 }
